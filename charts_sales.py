2f7d8c89dfbd6538b0cfefbc69d0db6368adb018
caseyhahn <casey.hahn@berkeley.edu>
2023-11-23 07:31:44 -0800

    minor updates

--- charts_sales.py
+++ charts_sales.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 import altair as alt
 import pandas as pd
 
-
-def display_chart_sales():
+def display_chart_country():
     file_path = "/home/casey.hahn/w209/static/DataCoSupplyChainDataset.csv"
     df = pd.read_csv(file_path,encoding="ISO-8859-1")
 
@@ -46,3 +45,66 @@ def display_chart_sales():
     chart_json = chart.to_json()
 
     return chart_json
+
+
+def display_chart_sales():
+    df_daily_file_path = "/home/casey.hahn/w209/static/df_daily.csv"
+    df_daily = pd.read_csv(df_daily_file_path,encoding="ISO-8859-1")
+    df_daily_file_products_path = "/home/casey.hahn/w209/static/df_daily_products.csv"
+    df_daily_products = pd.read_csv(df_daily_file_products_path,encoding="ISO-8859-1")
+
+    department_list = [None]
+    for item in df_daily['Department Name'].unique():
+        department_list.append(item)
+
+    department_labels = ["All"]
+    for item in df_daily['Department Name'].unique():
+        department_labels.append(item)
+
+    # Dropdown selection for department
+    department_dropdown = alt.binding_select(options=department_list, labels = department_labels, name="Department Name")
+    selection = alt.selection_point(fields=["Department Name"], bind = department_dropdown, value = None)
+
+    # Range slider for minimum and maximum year_month selection
+    brush = alt.selection_interval()
+
+    # Dropdown selection for year
+    year_list = [None]
+    for year in df_daily['year_month'].str[:4].unique():
+        year_list.append(int(year))
+
+    year_labels = ["All"]
+    for year in df_daily['year_month'].str[:4].unique():
+        year_labels.append(str(year))
+
+    year_dropdown = alt.binding_select(options=year_list, labels=year_labels, name="Year")
+    year_selection = alt.selection_point(fields=["year"], bind=year_dropdown, value = None)
+
+    # Chart with interactive selection
+    date_chart = alt.Chart(df_daily).mark_line().encode(
+        x=alt.X('year_month:N', title="")).add_params(brush).properties(title="Date Filter Slider", width=450).add_params(year_selection).transform_filter(year_selection)
+
+    chart_total = alt.Chart(df_daily).mark_line().encode(
+        x=alt.X('year_month:N', title=""),
+        y=alt.Y('sum(Order Item Quantity):Q', scale=alt.Scale(zero=False), title="Order Quantity"),
+        tooltip=[ 'sum(Order Item Quantity)'],
+    ).properties(width=400, height=200).add_params(
+        selection).transform_filter(selection).transform_filter(brush).add_params(year_selection).transform_filter(year_selection).properties(title="Items Sold")
+
+    chart_by_product = alt.Chart(df_daily_products).mark_line().encode(
+        x = alt.X('year_month:N',title = ""),
+        y = alt.Y('sum(Order Item Quantity):Q', scale = alt.Scale(zero=False), title = "Order Quantity"),
+        color = "Product_Name_top5_per_department_others",
+        tooltip=['Product_Name_top5_per_department_others', 'sum(Order Item Quantity)'],
+        ).properties(width = 400, height = 200).add_params(
+        selection).transform_filter(selection).transform_filter(brush).add_params(year_selection).transform_filter(year_selection).properties(title = "Items Sold by Product")
+
+
+    chart = (chart_total  | chart_by_product) & date_chart
+
+    # Convert Altair chart to JSON
+    chart_json = chart.to_json()
+
+    return chart_json
+
+
--- w209.py
+++ w209.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import altair as alt
 import pandas as pd
-from charts_sales import display_chart_sales
+from charts_sales import display_chart_country, display_chart_sales
 
 app = Flask(__name__)
 
@@ -17,42 +17,42 @@ def display_chart_sales_1():
 
 @app.route("/chart-sales-2")
 def display_chart_sales_2():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-sales-3")
 def display_chart_sales_3():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-customers-1")
 def display_chart_customers_1():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-customers-2")
 def display_chart_customers_2():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-customers-3")
 def display_chart_customers_3():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-delivery-1")
 def display_chart_delivery_1():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-delivery-2")
 def display_chart_delivery_2():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 @app.route("/chart-delivery-3")
 def display_chart_delivery_3():
-    chart_json = display_chart_sales()
+    chart_json = display_chart_country()
     return render_template("chart.html", chart_json=chart_json)
 
 if __name__ == "__main__":
